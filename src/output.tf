943ce96656bf717e76426bf4872b6b4612ab74cf
Flavio Milan <oflaviomilan@gmail.com>
2025-08-03 21:34:04 -0300

    chore: improve code

--- src/output.tf
+++ src/output.tf
@@ -1,11 +1,16 @@
 output "bucket_regional_domain_name" {
-  value = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+  value = var.domain_enabled ? aws_s3_bucket.s3_bucket[0].bucket_regional_domain_name : ""
 }
 
 output "bucket_id" {
-  value = aws_s3_bucket.s3_bucket.id
+  value = var.domain_enabled ? aws_s3_bucket.s3_bucket[0].id : ""
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.s3_bucket.arn
+  value = var.domain_enabled ? aws_s3_bucket.s3_bucket[0].arn : ""
+}
+
+output "name_servers" {
+  description = "Name servers for the Route 53 hosted zone."
+  value       = aws_route53_zone.public_zone.name_servers
 }
--- src/variables.tf
+++ src/variables.tf
@@ -13,3 +13,14 @@ variable "domain" {
     type        = string
     description = "The domain name for your application."
 }
+
+variable "files_path" {
+    type        = string
+    description = "The path to the files to be uploaded to the S3 bucket."
+}
+
+variable "domain_enabled" {
+  type        = bool
+  default     = false
+  description = "Enable or disable the creation of domain-related resources."
+}
